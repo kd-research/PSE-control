1603fb40e1523876d075e08cb3e5e7bddb25d9ca
Kaidong Hu <hukaidonghkd@gmail.com>
2023-04-12 17:33:21 -0400

    config userspec

--- lib/config_loader.rb
+++ lib/config_loader.rb
@@ -6,6 +6,8 @@ module ConfigLoader # :nodoc:
   private
 
   def load_config(path)
-    YAML.safe_load(ERB.new(File.read(path), trim_mode: '%<>').result, aliases: true)
+    loaded = YAML.safe_load(ERB.new(File.read(path), trim_mode: '%<>').result, aliases: true)
+    userspec = loaded.delete('userspec')
+    loaded.except('userspec').merge(userspec[ENV['USER']] || {})
   end
 end
