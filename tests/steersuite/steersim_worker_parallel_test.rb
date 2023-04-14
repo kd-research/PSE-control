6ca80a4a264d97484bfbe56cc1273f1d6ee9688b
Kaidong Hu <hukaidonghkd@gmail.com>
2023-04-14 12:02:32 -0400

    simulation test

--- tests/steersuite/steersim_worker_parallel_test.rb
+++ tests/steersuite/steersim_worker_parallel_test.rb
@@ -19,18 +19,39 @@ class SteersimWorkerParallelTest < Minitest::Test
                     end
     ) do
       SteerSuite.set_info('scene1')
-      1000.times do |_i|
+      100.times do |_i|
         p = ParameterObject.new(label: 'budget-ground', split: :train, state: :raw, file: nil)
         p.safe_set_parameter(9.times.map { rand })
         p.save!
       end
       SteerSuite.simulate_unsimulated
       assert_equal(0, ParameterObject.with_no_simulation.count)
-      assert_equal(1000, BenchmarkLogs.count)
+      assert_equal(100, BenchmarkLogs.count)
 
       SteerSuite.process_unprocessed
 
       assert_equal(0, ParameterObject.with_no_simulation.count)
+    end
+  end
+
+  %w[scene1 scene2 scene3 scene4].each do |scene|
+    define_method("test_if_auto_simulation_works_with_#{scene}") do
+      SteerSuite.set_info('scene1')
+
+      10.times do |i|
+        p = ParameterObject.new(label: 'budget-ground', split: :train, state: :raw, file: nil)
+        p.safe_set_parameter(SteerSuite.info.parameter_size.times.map { rand })
+        p.save!
+      end
+
+      SteerSuite.simulate_unsimulated
+
+      amount_unprocessed = ParameterObject.where.missing(:as_processor_relation).and(ParameterObject.raw).count
+      assert_equal(0, ParameterObject.with_no_simulation.count)
+      assert_equal(10, BenchmarkLogs.count)
+      assert_equal(10, amount_unprocessed)
+
+      SteerSuite.process_unprocessed
     end
   end
 end
