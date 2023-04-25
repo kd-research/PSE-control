b6511c645f1b78f7181d71cea9f5cb1388e7562d
Kaidong Hu <hukaidonghkd@gmail.com>
2023-04-24 20:08:36 -0400

    new scene generation

--- bin/scenario_feed.rb
+++ bin/scenario_feed.rb
@@ -1,21 +1,40 @@
 require 'tqdm'
+require 'fileutils'
 
 require_relative '../lib/steer_suite'
 require_relative '../lib/parameter_record/parameter_object'
 require_relative '../lib/parameter_record/parameter_object_relation'
 
-$target = :set_info
-ParameterDatabase.establish_connection(target: $target)
+$amount = 10000
+
+ParameterDatabase.establish_connection(target: :tmp)
 ParameterDatabase.initialize_database(force: true)
 
-SteerSuite.change_scene('sceneBasic4')
-if true
-  1000.times.tqdm.each do |i|
-    p = ParameterObject.new(label: 'budget-ground', split: :train, state: :raw, file: nil)
-    p.safe_set_parameter(21.times.map { rand })
-    p.save!
-  end
+def get_binary_filenames(dirname)
+  Dir.glob(File.join(dirname, "*.bin"))
 end
 
-SteerSuite.simulate_unsimulated
-SteerSuite.process_unprocessed
+%w[scene6].each do |scene|
+
+  SteerSuite.reinitialize!
+
+  SteerSuite.set_info(scene)
+  steersuite_config = SteerSuite.get_config.dup
+  steersuite_config['steersuite_record_pool'] = File.join(SteerSuite.info.data_location[:base], 'record')
+  steersuite_config['steersuite_process_pool'] = File.join(SteerSuite.info.data_location[:base], 'process')
+
+  SteerSuite.module_eval do
+    remove_const(:CONFIG)
+    const_set(:CONFIG, steersuite_config)
+  end
+
+  $amount.times.tqdm.each do
+    pobj = ParameterObject.new(split: :train, state: :raw, label: 'budget-ground')
+    pobj.safe_set_parameter(SteerSuite.info.parameter_size.times.map { rand })
+    pobj.save!
+  end
+
+  SteerSuite.simulate_unsimulated
+  SteerSuite.process_unprocessed
+
+end
--- lib/steersuite_utils/steersim_scene_info.rb
+++ lib/steersuite_utils/steersim_scene_info.rb
@@ -1,29 +1,31 @@
 # frozen_string_literal: true
 
 module SteerSuite
-class SteersimSceneInfo
-  def initialize(scene)
-    @scene = scene
-  end
+  class SteersimSceneInfo
+    def initialize(scene)
+      @scene = scene
+      @scene_config = CONFIG.dig('steersuite_scene', 'basic', @scene)
+    end
 
-  def parameter_size
-    CONFIG.dig('steersuite_scene', 'basic', @scene, 'parameter_size').to_i
-  end
+    def parameter_size
+      @scene_config['parameter_size'].to_i
+    end
 
-  def nagent
-    CONFIG.dig('steersuite_scene', 'basic', @scene, 'nagent').to_i
-  end
+    def nagent
+      @scene_config['nagent'].to_i
+    end
 
-  def data_location
-    base = StorageLoader.get_path("#{@scene}-base-data")
-    { train: File.join(base, 'train'), valid: File.join(base, 'valid'), test: File.join(base, 'test') }
-  end
+    def data_location
+      base = StorageLoader.get_path("#{@scene}-base-data")
+      { train: File.join(base, 'train'), valid: File.join(base, 'valid'), test: File.join(base, 'test'), base: base }
+    end
 
-  def prepare_steer_sim_config!
-    # based on scene name, config scene name is 'sceneBasic[1-4]'
-    scene_name = "sceneBasic#{@scene[-1]}"
-    SteerSuite::SteersimConfigEditor.change_scene(scene_name)
+    def prepare_steer_sim_config!
+      # based on scene name, config scene name is 'sceneBasic(%num)'
+      default_scene_name = "sceneBasic#{@scene[-1]}"
+      scene_name = @scene_config.fetch('scene_name', default_scene_name)
+      SteerSuite::SteersimConfigEditor.change_scene(scene_name)
+    end
   end
-end
 
 end
