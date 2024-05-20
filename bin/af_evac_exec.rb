08f785a8fd719c09df5977256fb8e9f992e60e5e
Kaidong Hu <dongforregister@gmail.com>
2024-05-19 21:08:29 -0400

    Update evacuation scenario setup and reduce GPU memory requirement in SLURM script
    Refactor af_evac_exec.rb to use command-line arguments for trial, scene,
    and subdir parameters, and set default agent number. Modify SteerSuite
    info setup and ActiveLearningCaller to exclude\
    keras_sample_train. In ruby_gpu_exec, adjust SLURM script to use less
    memory and specify Ruby version with RVM.

--- bin/af_evac_exec.rb
+++ bin/af_evac_exec.rb
@@ -7,10 +7,12 @@ require 'steer_suite'
 require 'active_learning'
 require 'parameter_object'
 
-$agent_num ||= "10"
+# 1 orca_1 identity
+trialnum = ARGV.shift
+scenenum = ARGV.shift
+subdir = ARGV.shift
 
-abort "Must specify subdir in evac scenario" unless $subdir
-SteerSuite.set_info("scene_evac_#{$scenenum}", subdir: $subdir)
+SteerSuite.set_info("scene_evac_#{scenenum}", subdir: "#{subdir}-rep0-record")
 
 ParameterDatabase.establish_connection
 ParameterDatabase.initialize_database(force: true)
@@ -24,10 +26,15 @@ valid_files = ParameterObject.where(split: :cross_valid, state: :valid_raw, labe
 renderer = AgentFormer.renderer_instance
 renderer.instance_variable_set :@segmented, '-evac'
 renderer.instance_variable_set :@num_epochs, 2
-renderer.instance_variable_set :@extra, "agent_num: #{$agent_num}\n"
+renderer.instance_variable_set :@extra, "agent_num: 10\n"
 renderer.set_data_source(train_files, valid_files, [])
 
 AgentFormer.call_agentformer
 AgentFormer.call_latent_dump
 ActiveLearningCaller.keras_train(segmented: true)
-ActiveLearningCaller.keras_sample_train(segmented: true)
+ActiveLearningCaller.keras_train(use_cpn: false, segmented: true)
+# ActiveLearningCaller.keras_sample_train(segmented: true)
+
+require 'rake'
+
+sh("ruby -Ilib ./bin/prediction_resimulate.rb #{trialnum},#{scenenum},#{subdir},#{Snapshot::SNAPSHOT_PATH}")
